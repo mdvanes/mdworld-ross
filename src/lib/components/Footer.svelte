08ccd98b8a8a9df75f76c8f4a4419d05b67c4815
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-04-27 13:44:51 +0200

    fix footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,13 +1,25 @@
 <script>
-	const currentYear = (new Date()).getFullYear();
+	const currentYear = new Date().getFullYear();
 </script>
 
 <footer>
 	<div class="footer-container main-column">
-		<a href="https://creativecommons.org/licenses/by-nc-sa/3.0/"
-			><img src="images/by-nc-sa.eu.svg" alt="creative commons" height="50px" />
-			<div>2003-{currentYear} MDWORLD</div></a
-		>
+		<div>
+			<a class="cc" href="https://creativecommons.org/licenses/by-nc-sa/3.0/"
+				><img src="images/by-nc-sa.eu.svg" alt="creative commons" height="50px" />
+				<div>2003-{currentYear} MDWORLD</div></a
+			>
+		</div>
+
+		<div>
+			<p>This site uses no tracking, analytics or cookies</p>
+			<p>
+				URLs to articles of previous versions of this blog are persistent since 2018 (<a
+					href="https://purl.archive.org/help">Why?</a
+				>)
+			</p>
+			<p>Hosted on <a href="https://pages.github.com">Github Pages</a>, domain at <a href="https://vdx.nl">VDX.nl</a></p>
+		</div>
 	</div>
 </footer>
 
@@ -26,9 +38,10 @@
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: center;
+		gap: 50px;
 	}
 
-	a {
+	a.cc {
 		display: flex;
 		text-decoration: none;
 		color: inherit;
