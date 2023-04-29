40132a9d337b0a0237e1e63ca2e26a083ee17d88
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-04-29 21:09:19 +0200

    fix build

--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -2,23 +2,24 @@ import type { MarkdownPost } from '$lib/utils/fetchMarkdownPosts.js';
 import { error } from '@sveltejs/kit';
 
 export const load = async ({ fetch, params }) => {
-	try {
-		const response = await fetch(`/api/posts/${params.slug}`);
-		const slugWithTimestamp: MarkdownPost = await response.json();
+  try {
+    // NOTE it would be nicer to have this under /api/posts/[slug] but this breaks SSG on the build
+    const response = await fetch(`/api/post/${params.slug}`);
+    const slugWithTimestamp: MarkdownPost = await response.json();
 
-		// NOTE if the .md file is renamed to not be prefixed with the timestamp, the md files will not be sorted in the dir
-		const post = await import(`../../posts/${slugWithTimestamp}.md`);
+    // NOTE if the .md file is renamed to not be prefixed with the timestamp, the md files will not be sorted in the dir
+    const post = await import(`../../posts/${slugWithTimestamp}.md`);
 
-		const { title, date, category } = post.metadata;
-		const content = post.default;
+    const { title, date, category } = post.metadata;
+    const content = post.default;
 
-		return {
-			content,
-			title,
-			date,
-			category
-		};
-	} catch (err) {
-		throw error(404, 'post not found ¯\\_(ツ)_/¯');
-	}
+    return {
+      content,
+      title,
+      date,
+      category
+    };
+  } catch (err) {
+    throw error(404, 'post not found ¯\\_(ツ)_/¯');
+  }
 };
