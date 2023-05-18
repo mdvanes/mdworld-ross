f5bead0fdc356fe8391a43fd09aad1342a38ccb2
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-05-18 12:45:23 +0200

    add og test

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -4,10 +4,27 @@
   export let data;
 </script>
 
-<MetaTags title="Example Title" description="Example Description." />
+<MetaTags
+  title="Example Title"
+  description="Example Description."
+  openGraph={{
+    url: 'https://mdworld.nl', // TODO
+    title: data.title,
+    description: 'Open Graph Description', // TODO
+    images: [
+      {
+        url: 'https://mdworld.nl/mdworld-ross/logos/logo-1024.png',
+        width: 1024,
+        height: 1024,
+        alt: 'MDWorld Logo'
+      }
+    ],
+    site_name: 'https://mdworld.nl'
+  }}
+/>
 
 <article>
-  <!-- TODO add header image -->
+  <!-- TODO add header image1 . -->
   <h1>{data.title}</h1>
   <p class="meta">
     <span>Published: {data.date}</span> <a href="/" class="pill">{data.category}</a>
