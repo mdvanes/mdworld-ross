468bff4baeb08204522cd6e07691febe1c4265ee
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-05-18 12:42:27 +0200

    add example meta tags

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,11 +1,16 @@
 <script>
-	export let data;
+  import { MetaTags } from 'svelte-meta-tags';
+
+  export let data;
 </script>
 
+<MetaTags title="Example Title" description="Example Description." />
+
 <article>
-	<h1>{data.title}</h1>
-	<p class="meta">
-		<span>Published: {data.date}</span> <a href="/" class="pill">{data.category}</a>
-	</p>
-	<svelte:component this={data.content} />
+  <!-- TODO add header image -->
+  <h1>{data.title}</h1>
+  <p class="meta">
+    <span>Published: {data.date}</span> <a href="/" class="pill">{data.category}</a>
+  </p>
+  <svelte:component this={data.content} />
 </article>
