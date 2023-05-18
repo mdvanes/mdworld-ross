6465cffbcef8496c07738f34ea8da7007ab1daa4
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-05-18 11:11:00 +0200

    add lightbox

--- src/lib/components/Lightbox.svelte
+++ src/lib/components/Lightbox.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
-	import '$lib/components/Lightbox.scss';
-	export let imgpath: string;
-	export let imgtitle: string;
+  import '$lib/components/Lightbox.scss';
 
-	const handleClick = () => {
-		alert('NYI');
-	};
+  export let imgpath: string;
+  export let imgtitle: string;
+
+  let lightboxDialog: HTMLDialogElement;
+
+  const handleClick = () => {
+    if (lightboxDialog) {
+      lightboxDialog.showModal();
+    }
+  };
+
+  const handleClose = () => {
+    if (lightboxDialog) {
+      lightboxDialog.close();
+    }
+  };
 </script>
 
 <button class="lightbox" on:click={handleClick}>
-	<img width="500" src={imgpath} alt={imgtitle} />
+  <img src={imgpath} alt={imgtitle} />
 </button>
+
+<dialog class="lightbox__dialog" bind:this={lightboxDialog}>
+  <div class="lightbox__dialog-header">
+    <h1>{imgtitle}</h1>
+    <div>
+      <button on:click={handleClose}>✕</button>
+    </div>
+  </div>
+  <img src={imgpath} alt={imgtitle} />
+</dialog>
